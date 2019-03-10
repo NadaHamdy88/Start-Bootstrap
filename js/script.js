8a5c45beddf22258b18d4ac788be948ce71776ac
nadahamdy88 <nada.hamdy7782@gmail.com>
2019-03-10 15:07:50 +0200

    start

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+$(document).ready(function(){
+
+    $(window).scroll(function(){
+        var w = $(this).scrollTop()
+        if ( w > 100) {
+            $("#mainNav").css({
+                'background-color':'white',
+                
+                
+            });
+            
+        } else {
+            $("#mainNav").css({
+                'background-color':
+                'rgba(34,34,34,.05)',
+               
+                
+            });
+        }
+    }); 
+});
+    
